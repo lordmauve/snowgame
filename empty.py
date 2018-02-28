710da176e11fc415ebb331151b41e42b3db8c8fe
Dario Boscaratto <lcars89@gmail.com>
2018-02-28 20:49:01 +0000

    drawing lines

--- empty.py
+++ empty.py
@@ -6,12 +6,17 @@ WIDTH = 350
 HEIGHT = 200
 BOTTOM_COLOUR = (255,0,0)
 BLACK = (0, 0, 0)
+drag_start = None
+current_mouse = None
+
 snow_color = (255, 255, 255)
+guide_line = (0, 255, 0)
 snow_particles = []
 settled = Surface((WIDTH, HEIGHT))
 
-logo = Actor('python', center=(WIDTH / 2, HEIGHT / 2))
-settled.blit(images.python, logo.topleft)
+#
+# logo = Actor('python', center=(WIDTH / 2, HEIGHT / 2))
+# settled.blit(images.python, logo.topleft)
 
 line(settled, BOTTOM_COLOUR, (0, HEIGHT - 1), (WIDTH / 2 - 10, HEIGHT - 1))
 line(settled, BOTTOM_COLOUR, (WIDTH, HEIGHT - 1), (WIDTH / 2 + 10, HEIGHT - 1))
@@ -41,6 +46,22 @@ def update():
             pixels.append((x, nexty))
     snow_particles[:] = pixels
 
+def on_mouse_down(pos):
+    global drag_start
+    print(drag_start, pos)
+    drag_start = pos
+
+def on_mouse_up(pos):
+    global drag_start
+    line(settled, BOTTOM_COLOUR, drag_start, pos)
+    drag_start = None
+    current_mouse = None
+
+def on_mouse_move(pos, rel, buttons):
+    global current_mouse
+    current_mouse = pos
+
+
 def create_particle():
     x = random.randrange(0, WIDTH)
     y = 0
@@ -50,5 +71,8 @@ def draw():
     screen.blit(settled, (0, 0))
     for part in snow_particles:
         screen.surface.set_at(part, snow_color)
+
+    if drag_start:
+        line(screen.surface, guide_line, drag_start, current_mouse)
 
 clock.schedule_interval(create_particle, .01)
