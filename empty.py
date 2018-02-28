ce57b77c5ab650a0ef04b5abc8d9315abde4ffe5
Dario Boscaratto <lcars89@gmail.com>
2018-02-28 20:02:19 +0000

    Update

--- empty.py
+++ empty.py
@@ -1,22 +1,31 @@
 import random
+from pygame import Surface
 
 WIDTH = 300
+HEIGHT = 600
+snow_color = (255, 255, 255)
 snow_particles = []
+settled = Surface((WIDTH, HEIGHT))
 
 def update():
     pixels = []
     for x, y in snow_particles:
-        pixels.append((x, y+1))
+        if y+1 > HEIGHT:
+            continue
+        if y+1 == HEIGHT or settled.get_at((x, y+1)) == snow_color:
+            settled.set_at((x,y), snow_color)
+        else:
+            pixels.append((x, y+1))
     snow_particles[:] = pixels
 
 def create_particle():
-    x = random.randint(0, WIDTH)
+    x = random.randrange(0, WIDTH)
     y = 0
     snow_particles.append((x, y))
 
 def draw():
-    screen.fill('blue')
+    screen.blit(settled, (0, 0))
     for part in snow_particles:
-        screen.surface.set_at(part, (255, 255, 255))
+        screen.surface.set_at(part, snow_color)
 
 clock.schedule_interval(create_particle, .05)
